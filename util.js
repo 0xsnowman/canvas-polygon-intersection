8275f1691c980fa90193cbfc0e30909882769955
snowman <eth.snowman@gmail.com>
2025-02-28 07:09:24 -0300

    points-count-for-each-angle-fix

--- util.js
+++ util.js
@@ -14,7 +14,7 @@ function toRadians(angle) {
 
 // get needed points count for certain angle
 function getPointsCountForAngle(ridians) {
-  return Math.round(2 / ridians) + 3;
+  return Math.round(ridians * 4) + 3;
 }
 
 // generates random camera ID
